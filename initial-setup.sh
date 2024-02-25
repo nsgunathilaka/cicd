44504f5224db070352e874c66cab45478259b7af
nsgunathilaka <nihalshantha470@gmail.com>
2024-02-25 14:32:56 +0530

    initial

---
+++ initial-setup.sh
@@ -0,0 +1,22 @@
+sudo apt-get update -y
+sudo apt install openjdk-8-jdk -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install fontconfig openjdk-17-jre
+sudo apt-get install jenkins
+
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+
